f521d956be7afb59d43a9d38816230fcaf1658a9
Loreto Dumitrescu <loretodalt@gmail.com>
2021-06-30 14:22:09 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.touchD2.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("d", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
+})
+input.touchD1.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("s", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
+})
+input.touchD0.onEvent(ButtonEvent.Click, function () {
+    keyboard.key("a", KeyboardKeyEvent.Press)
+    keyboard.clearAllKeys()
+})
